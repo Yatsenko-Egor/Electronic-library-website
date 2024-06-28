3cb519d2996fe2313bd11025d927bbf6856aed4d
Yatsenko-Egor <74555653+Yatsenko-Egor@users.noreply.github.com>
2024-06-28 10:21:49 +0300

    Добавлена первая страница сайта

---
+++ scripts/new_books_loader.js
@@ -0,0 +1,29 @@
+fetch('../data/new_books.json')
+                .then(response => response.json())
+                .then(data => {
+                    const list = document.getElementById('new-books');
+                    data.books.forEach(book => {
+                        const item = document.createElement('div');
+                        item.classList.add('book');
+                        
+                        const image = document.createElement('img');
+                        image.src = "../img/new_books/" + book.image;
+                        image.alt = book.title;
+                        image.classList.add('image');
+                        
+                        const title = document.createElement('h3');
+                        title.textContent = book.title;
+                        title.classList.add('title');
+                        
+                        const author = document.createElement('p');
+                        author.textContent = book.author;
+                        author.classList.add('author');
+                        
+                        item.appendChild(image);
+                        item.appendChild(title);
+                        item.appendChild(author);
+                        
+                        list.appendChild(item);
+                    });
+                })
+                .catch(error => console.error('Ошибка загрузки данных:', error));
